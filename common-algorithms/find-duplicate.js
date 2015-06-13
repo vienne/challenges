a93dd35778a042d3bed36ec5082a3eb60052f927
Vienne Chen <vienne.chen@gmail.com>
2015-06-13 18:28:26 -0400

    adding two more algorithms

---
+++ common-algorithms/find-duplicate.js
@@ -0,0 +1,22 @@
+//i. Given an array of integers between 1 and 1,000,000. One integer is in the twice. Find the duplicate.
+
+function findDuplicate(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++){
+      if (arr[i] == arr[j] && i != j ) {
+        return arr[i];
+      }
+    }
+  }
+}
+
+
+function assert(condition, desc) {
+  var result = condition ? "pass" : "fail";
+  console.log(result + ': ' +  desc);
+}
+
+var resultPass = findDuplicate([1,2,3,4,5,5]);
+var resultFail = findDuplicate([1,2,3,3,4,5]);
+assert(result == 5, 'Checking findDuplicate function');
+assert(result == 4, 'Checking findDuplicate function');
---
+++ common-algorithms/first-nonrepeating.js
@@ -0,0 +1,28 @@
+//ii. Find the first non-repeating character in a string:("DEFD" -> E)
+
+function findFirstUnique(str) {
+  var st = str.toLowerCase();
+  var notUniq;
+  for (var i = 0; i < st.length; i++) {
+    notUniq = false;
+    for (var j = 0; j < st.length; j++) {
+      if (i != j && st[i] == st[j]) {
+        notUniq = true;
+        break;
+      }
+    }
+    if (notUniq === false) {
+      return st[i];
+    }
+  }
+}
+
+function assert(condition, desc) {
+  var result = condition ? "pass" : "fail";
+  console.log(result + ': ' +  desc);
+}
+
+var testPass = findFirstUnique("AbcBa");
+var testFail = findFirstUnique("abcdefabcdf");
+assert(testResult == "c", 'Checking findFirstUnique function');
+assert(testResult == "a", 'Checking findFirstUnique function');

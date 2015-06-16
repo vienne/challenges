95b82c7ac046468315755c538a03c6ab218e957f
Vienne Chen <vienne.chen@gmail.com>
2015-06-16 15:08:08 -0400

    merge two sorted arays

---
+++ common-algorithms/merge-sorted-arrays.js
@@ -0,0 +1,55 @@
+//merge two sorted arrays
+
+
+function mergeArrays(arr1, arr2) {
+  var combinedArray = [];
+  var num, num2;
+
+  while(arr1.length && arr2.length){
+      if (arr1[0] < arr2[0]) {
+        num = arr1.shift();
+        combinedArray.push(num);
+      } else if (arr2[0] < arr1[0]) {
+        num = arr2.shift();
+        combinedArray.push(num);
+      } else if (arr1[0] == arr2[0]) {
+        num = arr1.shift();
+        num2 = arr2.shift();
+        combinedArray.push(num);
+        combinedArray.push(num2);
+      }
+  }
+      while (arr1.length) {
+        num = arr1.shift();
+        combinedArray.push(num);
+      }
+      while (arr2.length) {
+        num = arr2.shift();
+        combinedArray.push(num);
+      }
+
+  return combinedArray;
+}
+
+function compareArrays(a1, a2) {
+  if (a1.length != a2.length) {
+    return false;
+  }
+  for (var i = 0; i < a1.length; i++) {
+    if (a1[i] != a2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function assert(cond, desc) {
+  var result = cond ? "pass" : "fail";
+  console.log(result + " : " + desc);
+}
+
+var test1 = mergeArrays([1,3,5,11,21], [2,6,7,18]);
+var test2 = mergeArrays([19,150,200,500], [150,201,501,1000,100000]);
+
+assert(compareArrays(test1, [1, 2, 3, 5, 6, 7, 11, 18, 21]), "should merge two sorted arrays");
+assert(compareArrays(test2, [19,150,150,200,201,500,501,1000,100000]), "should merge two sorted arrays");
